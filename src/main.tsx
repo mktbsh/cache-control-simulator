66589fb0152da42a5037aa2f31d9cd68348fd6f9
mktbsh <59120037+mktbsh@users.noreply.github.com>
2025-03-26 01:01:24 +0900

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+
+async function main() {
+	const selector = "#root";
+	const target = document.querySelector(selector);
+	if (!target) throw new Error(`Could not found: ${selector}`);
+
+	return ReactDOM.createRoot(target).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
+}
+
+main();
